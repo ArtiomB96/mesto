6461d098ec4b811ed949b54326c5c73d8dfae4a8
Artiom Brantov <www.arem@mail.ru>
2022-10-24 12:54:56 +0700

    Переименовал функции и переменные в js

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 //Код открытия попапа по нажатию на кнопку edit- button
-const buttonElem = document.querySelector('.kusto__edit-button'); //нахожу в обработчике кнопку, с которой работаю
+const buttonPopupOpen = document.querySelector('.kusto__edit-button'); //нахожу в обработчике кнопку, с которой работаю
 const overlay = document.querySelector('.overlay-form'); //оверлэй, который буду менять
 const popup = document.querySelector('.edit-form'); //форма, которую буду менять
 const buttonSave = document.querySelector('.edit-form__save');
@@ -12,19 +12,19 @@ const buttonLike = document.querySelector('.photo-grid__like-button');
 const imageLike = document.querySelector('.photo-grid__like');
 
 //создаю функцию, которая будет менять классы оверлэя и попапа
-function onButtonClick() {
+function openPopup() {
     overlay.classList.add('overlay-form_open'); //добавил оверлэй при открытом попап
     popup.classList.add('edit-form_open'); //добавил открытый попап
 
 }
-function onButtonClose() {
+function closePopup() {
     overlay.classList.remove('overlay-form_open'); //добавил оверлэй при открытом попап
     popup.classList.remove('edit-form_open'); //добавил открытый попап
 
 }
 // Код сохранения информации в блок кусто профиль инфо
 
-function onButtonSave(evt) {
+function save(evt) {
     evt.preventDefault();
     titleElem.textContent = inputName.value;
     subtitleElem.textContent = inputInfo.value;
@@ -35,12 +35,12 @@ function onButtonSave(evt) {
 }
 //Код нажатия на кнопку лайка
 
-function onButtonLike(buttonLike) {
+function like() {
     imageLike.classList.toggle('photo-grid__like_black');
 }
 
-buttonElem.addEventListener('click', onButtonClick); //связал команду клик и функцию замены классов по клику
-buttonClose.addEventListener('click', onButtonClose); //связал команду клик и функцию замены классов по клику
-buttonSave.addEventListener('click', onButtonSave);
-buttonLike.addEventListener('click', onButtonLike);
+buttonPopupOpen.addEventListener('click', openPopup); //связал команду клик и функцию замены классов по клику
+buttonClose.addEventListener('click', closePopup); //связал команду клик и функцию замены классов по клику
+buttonSave.addEventListener('click', save);
+buttonLike.addEventListener('click', like);
 console.log(buttonLike)
