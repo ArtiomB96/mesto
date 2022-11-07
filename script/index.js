a4b5a3e992656397019f51f107de601a0459fd8b
Artiom Brantov <www.arem@mail.ru>
2022-11-07 17:08:52 +0700

    Сделал, чтоб открывались 2 разных попапа, используя 1 функцию с разными аргументами. Переименовал edit-form в form.

--- script/index.js
+++ script/index.js
@@ -1,25 +1,31 @@
 //Код открытия попапа по нажатию на кнопку edit- button
-const buttonPopupOpen = document.querySelector('.kusto__edit-button'); //нахожу в обработчике кнопку, с которой работаю
-const overlay = document.querySelector('.overlay-form'); //оверлэй, который буду менять
-const popup = document.querySelector('.edit-form'); //форма, которую буду менять
-const buttonSave = document.querySelector('.edit-form__save');
-const buttonClose = document.querySelector('.edit-form__close');
+const popupEditButton = document.querySelector('.kusto__edit-button'); //нахожу в обработчике кнопку, с которой работаю
+const overlay = document.querySelectorAll('.overlay-form'); //оверлэй, который буду менять
+const popup = document.querySelectorAll('.popup'); //форма, которую буду менять
+const editPopup = document.querySelector('.edit-popup');
+const editOverlay = document.querySelector('.edit-overlay');
+const buttonSave = document.querySelector('.form__save');
+const editPopupClose = document.querySelector('.edit-close');
+const addPopupClose = document.querySelector('.add-close');
 const titleElem = document.querySelector('.kusto__title');
 const subtitleElem = document.querySelector('.kusto__subtitle');
-const inputName = document.querySelector('.edit-form__field_name');
-const inputInfo = document.querySelector('.edit-form__field_info');
+const inputName = document.querySelector('.form__field_name');
+const inputInfo = document.querySelector('.form__field_info');
 const buttonLike = document.querySelector('.photo-grid__like-button');
 const imageLike = document.querySelector('.photo-grid__like');
+const popupAddButton = document.querySelector('.kusto__add-button'); //нахожу в обработчике кнопку, с которой работаю
+const addPopup = document.querySelector('.add-popup');
+const addOverlay = document.querySelector('.add-overlay');
 
 //создаю функцию, которая будет менять классы оверлэя и попапа
-function openPopup() {
+function openPopup(overlay, popup) {
     overlay.classList.add('overlay-form_open'); //добавил оверлэй при открытом попап
-    popup.classList.add('edit-form_open'); //добавил открытый попап
+    popup.classList.add('popup_open'); //добавил открытый попап
 
 }
-function closePopup() {
-    overlay.classList.remove('overlay-form_open'); //добавил оверлэй при открытом попап
-    popup.classList.remove('edit-form_open'); //добавил открытый попап
+function closePopup(overlay, popup) {
+    overlay.classList.remove('overlay-form_open'); 
+    popup.classList.remove('popup_open'); 
 
 }
 // Код сохранения информации в блок кусто профиль инфо
@@ -28,8 +34,8 @@ function save(evt) {
     evt.preventDefault();
     titleElem.textContent = inputName.value;
     subtitleElem.textContent = inputInfo.value;
-    overlay.classList.remove('overlay-form_open'); //закрыть оверлэй
-    popup.classList.remove('edit-form_open'); //закрыть попап
+    editOverlay.classList.remove('overlay-form_open'); //закрыть оверлэй
+    editPopup.classList.remove('popup_open'); //закрыть попап
 
 
 }
@@ -39,8 +45,19 @@ function like() {
     imageLike.classList.toggle('photo-grid__like_black');
 }
 
-buttonPopupOpen.addEventListener('click', openPopup); //связал команду клик и функцию замены классов по клику
-buttonClose.addEventListener('click', closePopup); //связал команду клик и функцию замены классов по клику
+popupEditButton.addEventListener('click', function () {
+    openPopup(editOverlay, editPopup)
+}); //связал команду клик и функцию замены классов по клику
+editPopupClose.addEventListener('click', function () {
+    closePopup(editOverlay, editPopup)
+}); //связал команду клик и функцию замены классов по клику
+
+popupAddButton.addEventListener('click', function () {
+    openPopup(addOverlay, addPopup)
+}); //связал команду клик и функцию замены классов по клику
+addPopupClose.addEventListener('click', function () {
+    closePopup(addOverlay, addPopup)
+}); //связал команду клик и функцию замены классов по клику
 buttonSave.addEventListener('click', save);
 buttonLike.addEventListener('click', like);
 console.log(buttonLike)
